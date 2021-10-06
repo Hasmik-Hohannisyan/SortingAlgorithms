236ab3209b9aa3196eee70ed5b82aa39dbd90e9d
Hasmik-Hohannisyan <82596963+Hasmik-Hohannisyan@users.noreply.github.com>
2021-10-06 11:52:21 +0400

    Add project files.

---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Please enter the size of an array that you want to sort: ");
+
+                bool isParse = false;
+
+                if (int.TryParse(Console.ReadLine(), out int n))
+                {
+                    isParse = true;
+                }
+
+                if (isParse)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Select which algorithm you want to perform:");
+                    Console.WriteLine("1.Bubble sort, \n2.Insertion sort, \n3.Shell sort, \n4.Heap sort, \n5.Marge sort, \n6.Quick sort, \n7.All:");
+                    while (true)
+                    {
+
+                        Console.WriteLine("Selection: ");
+
+                        string algoritmCount = Console.ReadLine();
+                        Console.WriteLine();
+
+                        int[] numbers = new int[n];
+                        RandomDigits(numbers);
+
+                        List<Sort> sortList = new List<Sort>();
+
+                        string[] symbols = algoritmCount.Split(" 1234567".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                        List<int> num = new List<int>();
+                        string[] numberAlgoritms = algoritmCount.Split(" -,".ToCharArray());
+
+                        bool isDigit = true;
+                        if (!numberAlgoritms.Contains("1") && !numberAlgoritms.Contains("2") && !numberAlgoritms.Contains("3") && !numberAlgoritms.Contains("4")
+                            && !numberAlgoritms.Contains("5") && !numberAlgoritms.Contains("6") && !numberAlgoritms.Contains("7"))
+                        {
+                            isDigit = false;
+                        }
+
+                        if (isDigit)
+                        {
+                            foreach (string number in numberAlgoritms)
+                            {
+                                num.Add(int.Parse(number));
+                            }
+                        }
+                        else
+                        {
+
+                            Console.WriteLine("This does not match the format, please enter numbers.");
+                            Console.WriteLine();
+                            continue;
+                        }
+
+
+                        long size1Memory = default;
+                        long size2Memory = default;
+
+
+                        if (symbols.Contains("-"))
+                        {
+                            for (int i = num[0]; i <= num[num.Count - 1]; i++)
+                            {
+                                ListObjects(i, sortList);
+                                sortList[i - num[0]].SortName();
+
+                                size1Memory = GC.GetTotalMemory(false);
+                                sortList[i - num[0]].Sorting((int[])numbers.Clone());
+                                size2Memory = GC.GetTotalMemory(false);
+
+                                Console.WriteLine($"Memory size 1: { size1Memory}");
+                                Console.WriteLine($"Memory size 2: { size2Memory}");
+                                Console.WriteLine($"Memory size: { size2Memory - size1Memory}");
+                                Console.WriteLine();
+                            }
+                        }
+                        if (symbols.Contains(","))
+                        {
+                            for (int i = 0; i < num.Count; i++)
+                            {
+                                ListObjects(num[i], sortList);
+                                sortList[i].SortName();
+
+                                size1Memory = GC.GetTotalMemory(false);
+                                sortList[i].Sorting((int[])numbers.Clone());
+                                size2Memory = GC.GetTotalMemory(false);
+
+                                Console.WriteLine($"Memory size 1: { size1Memory}");
+                                Console.WriteLine($"Memory size 2: { size2Memory}");
+                                Console.WriteLine($"Memory size: { size2Memory - size1Memory}");
+                                Console.WriteLine();
+                            }
+                        }
+
+                        switch (algoritmCount)
+                        {
+                            case "1":
+                                ImplementationForNumberOne(sortList, numbers, algoritmCount, size1Memory, size2Memory);
+                                break;
+                            case "2":
+                                ImplementationForNumberOne(sortList, numbers, algoritmCount, size1Memory, size2Memory);
+                                break;
+                            case "3":
+                                ImplementationForNumberOne(sortList, numbers, algoritmCount, size1Memory, size2Memory);
+                                break;
+                            case "4":
+                                ImplementationForNumberOne(sortList, numbers, algoritmCount, size1Memory, size2Memory);
+                                break;
+                            case "5":
+                                ImplementationForNumberOne(sortList, numbers, algoritmCount, size1Memory, size2Memory);
+                                break;
+                            case "6":
+                                ImplementationForNumberOne(sortList, numbers, algoritmCount, size1Memory, size2Memory);
+                                break;
+                            case "7":
+                                for (int i = 1; i <= 6; i++)
+                                {
+                                    ImplementationForNumberAll(sortList, numbers, i.ToString(), size1Memory, size2Memory);
+                                    Console.WriteLine();
+                                }
+                                break;
+                        }
+
+                        Console.ReadKey();
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("This does not match the format, please enter numbers.");
+                }
+            }
+        }
+
+        private static void ImplementationForNumberOne(List<Sort> sortList, int[] numbers, string algoritmCount, long size1Memory, long size2Memory)
+        {
+            ListObjects(int.Parse(algoritmCount), sortList);
+            sortList[0].SortName();
+
+            size1Memory = GC.GetTotalMemory(false);
+            sortList[0].Sorting((int[])numbers.Clone());
+            size2Memory = GC.GetTotalMemory(false);
+
+            Console.WriteLine($"Memory size 1: { size1Memory}");
+            Console.WriteLine($"Memory size 2: { size2Memory}");
+            Console.WriteLine($"Memory size: { size2Memory - size1Memory}");
+            Console.WriteLine();
+        }
+        private static void ImplementationForNumberAll(List<Sort> sortList, int[] numbers, string algoritmCount, long size1Memory, long size2Memory)
+        {
+            ListObjects(int.Parse(algoritmCount), sortList);
+            sortList[int.Parse(algoritmCount) - 1].SortName();
+
+            size1Memory = GC.GetTotalMemory(false);
+            sortList[int.Parse(algoritmCount) - 1].Sorting((int[])numbers.Clone());
+            size2Memory = GC.GetTotalMemory(false);
+
+            Console.WriteLine($"Memory size 1: { size1Memory}");
+            Console.WriteLine($"Memory size 2: { size2Memory}");
+            Console.WriteLine($"Memory size: { size2Memory - size1Memory}");
+            Console.WriteLine();
+        }
+
+
+        static void ListObjects(int i, List<Sort> sortList)
+        {
+            switch (i)
+            {
+                case 1:
+                    Sort sortBubble = new Bubble();
+                    sortList.Add(sortBubble);
+                    break;
+                case 2:
+                    Sort sortInsertion = new Insertion();
+                    sortList.Add(sortInsertion);
+                    break;
+                case 3:
+                    Sort sortShell = new Shell();
+                    sortList.Add(sortShell);
+                    break;
+                case 4:
+                    Sort sortHeap = new Heap();
+                    sortList.Add(sortHeap);
+                    break;
+                case 5:
+                    Sort sortMerge = new Merge();
+                    sortList.Add(sortMerge);
+                    break;
+                case 6:
+                    Sort sortQuick = new Quick();
+                    sortList.Add(sortQuick);
+                    break;
+            }
+        }
+        static void RandomDigits(int[] numbers)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rand.Next(1, 99);
+            }
+
+        }
+    }
+}
---
+++ SortingAlgorithms/Sort.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+
+namespace SortingAlgorithms
+{
+    abstract class Sort
+    {
+        internal abstract void Sorting(int[] numbers);
+        internal abstract void SortName();
+        internal Stopwatch stopWatch = new Stopwatch();
+
+        static internal void Print(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+
+    }
+
+    class Insertion : Sort
+    {
+        internal override void SortName()
+        {
+            Console.WriteLine(" Insertion Sort: ");
+        }
+        internal override void Sorting(int[] numbers)
+        {
+           stopWatch.Start();
+        
+            for (int i = 1; i < numbers.Length; ++i)
+            {
+                int key = numbers[i];
+                int j = i - 1;
+
+                while (j >= 0 && numbers[j] > key)
+                {
+                    numbers[j + 1] = numbers[j];
+                    j--;
+                }
+                numbers[j + 1] = key;
+            }
+
+            stopWatch.Stop();
+
+            Sort.Print(numbers);
+            Console.WriteLine();
+            Console.WriteLine($"Time elapsed: { stopWatch.Elapsed}"); 
+        }
+
+       
+    }
+    class Bubble : Sort
+    {
+        internal override void SortName()
+        {
+            Console.WriteLine(" Bubble Sort: ");
+        }
+        internal override void Sorting(int[] numbers)
+        {
+            stopWatch.Start();
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                bool swapped = false;
+                for (int j = 0; j < numbers.Length - i - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                if (!swapped)
+                {
+                    break;
+                }
+            }
+            stopWatch.Stop();
+
+            Sort.Print(numbers);
+            Console.WriteLine();
+            Console.WriteLine($"Time elapsed: { stopWatch.Elapsed}");
+        }
+    }
+    class Shell : Sort
+    {
+        internal override void SortName()
+        {
+            Console.WriteLine(" Shell Sort: ");
+        }
+        internal override void Sorting(int[] numbers)
+        {
+            stopWatch.Start();
+            int n = numbers.Length;
+           
+            for (int gap = n / 2; gap > 0; gap /= 2)
+            {
+                for (int i = gap; i < n; i += 1)
+                {
+                    int temp = numbers[i];
+                    int j;
+                    for (j = i; j >= gap && numbers[j - gap] > temp; j -= gap)
+                        numbers[j] = numbers[j - gap];
+                    numbers[j] = temp;
+                }
+            }
+            stopWatch.Stop();
+
+            Sort.Print(numbers);
+            Console.WriteLine();
+            Console.WriteLine($"Time elapsed: { stopWatch.Elapsed}");
+        }
+    }
+
+
+    class Quick : Sort
+    {
+        internal override void SortName()
+        {
+            Console.WriteLine(" Quick Sort: ");
+        }
+        internal override void Sorting(int[] numbers)
+        {
+            stopWatch.Start();
+            SortQuick(numbers, 0, numbers.Length - 1);
+            stopWatch.Stop();
+
+            Sort.Print(numbers);
+            Console.WriteLine();
+            Console.WriteLine($"Time elapsed: { stopWatch.Elapsed}");
+        }
+
+        void SortQuick(int[] numbers, int left, int right)
+        {
+           
+            if (left < right)
+            {
+                int pivot = Partition(numbers, left, right);
+
+                if (pivot > 1)
+                    SortQuick(numbers, left, pivot - 1);
+
+                if (pivot + 1 < right)
+                    SortQuick(numbers, pivot + 1, right);
+            }
+
+        }
+        static public int Partition(int[] numbers, int left, int right)
+        {
+            int pivot = numbers[left];
+
+            while (true)
+            {
+                while (numbers[left] < pivot)
+                    left++;
+
+                while (numbers[right] > pivot)
+                    right--;
+
+                if (left < right)
+                {
+                    int temp = numbers[right];
+                    numbers[right] = numbers[left];
+                    numbers[left] = temp;
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+    }
+
+    class Merge : Sort
+    {
+        internal override void SortName()
+        {
+            Console.WriteLine(" Merge Sort: ");
+        }
+        internal override void Sorting(int[] numbers)
+        {
+            stopWatch.Start();
+            sort(numbers, 0, numbers.Length - 1);
+            stopWatch.Stop();
+
+            Sort.Print(numbers);
+            Console.WriteLine();
+            Console.WriteLine($"Time elapsed: { stopWatch.Elapsed}");
+        }
+
+        void sort(int[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = l + (r - l) / 2;
+                sort(arr, l, m);
+                sort(arr, m + 1, r);
+                merge(arr, l, m, r);
+            }
+        }
+        void merge(int[] arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+
+            for (i = 0; i < n1; ++i)
+                L[i] = arr[l + i];
+            for (j = 0; j < n2; ++j)
+                R[j] = arr[m + 1 + j];
+
+            i = 0;
+            j = 0;
+
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+    }
+    class Heap : Sort
+    {
+        internal override void SortName()
+        {
+            Console.WriteLine(" Heap Sort: ");
+        }
+        internal override void Sorting(int[] numbers)
+        {
+            stopWatch.Start();
+
+            int n = numbers.Length;
+            for (int i = n / 2 - 1; i >= 0; i--)
+                heapify(numbers, n, i);
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                int temp = numbers[0];
+                numbers[0] = numbers[i];
+                numbers[i] = temp;
+
+                heapify(numbers, i, 0);
+            }
+            stopWatch.Stop();
+
+            Sort.Print(numbers);
+            Console.WriteLine();
+            Console.WriteLine($"Time elapsed: { stopWatch.Elapsed}");
+        }
+        void heapify(int[] arr, int n, int i)
+        {
+            int largest = i; 
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+
+            if (l < n && arr[l] > arr[largest])
+                largest = l;
+
+            if (r < n && arr[r] > arr[largest])
+                largest = r;
+
+            if (largest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+
+                heapify(arr, n, largest);
+            }
+        }
+
+    }
+}
